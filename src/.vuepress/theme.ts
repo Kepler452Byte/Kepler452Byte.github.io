a34cd9517b9523c8ae6a990e3546beb05e82eb2a
sx-yuanhong.shao <sx-yuanhong.shao@clickzetta.com>
2025-08-23 02:34:12 +0800

    bugfix

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -4,7 +4,7 @@ import { enNavbar, zhNavbar } from "./navbar/index.js";
 import { enSidebar, zhSidebar } from "./sidebar/index.js";
 
 export default hopeTheme({
-  hostname: "https://Kepler452Byte.github.io",
+  hostname: "https://kepler452byte.github.io",
 
   author: {
     name: "Kepler452Byte",
